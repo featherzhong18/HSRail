2a3f06ba8e4e08604bf2da0c8fae6ffed90dfbef
JaarLiu <1614314785@qq.com>
2021-12-29 00:17:35 +0800

    车票

--- HSRail/Order.h
+++ HSRail/Order.h
@@ -8,7 +8,7 @@
 
 typedef struct Order {
     int number;        // 订单号
-    User us;         // 乘车人
+    User user;         // 乘车人
     TicketInfo ticket;//车票信息
     char* date;        // 买票日期
     int condition;     // 票的状态（有效和无效）
