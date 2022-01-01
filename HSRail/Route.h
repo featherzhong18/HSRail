0b048ac2a45025e3a69886e1587556ac18171b4d
sebastianZhong18 <73680040+featherzhong18@users.noreply.github.com>
2022-01-01 21:48:44 +0800

    Update Route.h
    修改时间类型float

--- HSRail/Route.h
+++ HSRail/Route.h
@@ -4,6 +4,6 @@
 typedef struct Route {
     String* places;   // 路线中的所有地点
     int* length;         // 该路线的长度
-    int* cost;           // 费用
-    Date* time;           // 用时
+    int* price;           // 费用
+    float* time;         // 用时
 } Route;
