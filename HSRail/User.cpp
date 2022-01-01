5285680448043dd71ed4e71413d67642da8469b2
featherzhong18 <2725072799@qq.com>
2022-01-01 15:16:00 +0800

    zyf2

--- HSRail/User.cpp
+++ HSRail/User.cpp
@@ -1,7 +1,5 @@
 #include "User.h"
 
-<<<<<<< HEAD
-=======
 Status InitUserList(UserList& U) {//初始化用户链表
 	U = (UserList)malloc(sizeof(UserNode));
 	if (!U) return ERROR;
@@ -9,4 +7,3 @@ Status InitUserList(UserList& U) {//初始化用户链表
 	U->next = NULL;
 	return OK;
 }
->>>>>>> 90471926657ce6bfa739bf557e9cb626d79ce394
