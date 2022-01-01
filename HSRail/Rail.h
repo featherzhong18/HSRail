90471926657ce6bfa739bf557e9cb626d79ce394
JaarLiu <1614314785@qq.com>
2022-01-01 14:56:09 +0800

    车次链表相关函数

--- HSRail/Rail.h
+++ HSRail/Rail.h
@@ -11,5 +11,8 @@ typedef struct Rail {
     struct Rail* next;
 } RailNode, *RailList;
 
-Status InitRailList(RailList &R);
-RailNode* MakeRailNode(int id, Route route, String date, int num);
+Status InitRailList(RailList &R);//初始化车次链表
+RailNode* MakeRailNode(int id, Route route, String date, int num);//创建车次结点
+Status InsertRailNode(RailNode* p, RailNode* q);//插入车次结点
+Status DeleteRailNode(RailNode* p, int& id);//删除车次结点
+Status CreateRailList(RailList& R, int n, int* id, Route* route, String* date, int* num);//创建车次链表
