790c294c9fb803e3546489f76e363229a902d073
featherzhong18 <2725072799@qq.com>
2022-01-01 15:16:19 +0800

    zyf2

--- HSRail/HSRail.cpp
+++ HSRail/HSRail.cpp
@@ -5,13 +5,11 @@
 #include "User.h"
 
 int main() {
-<<<<<<< HEAD
 	/*scanf_s("%s", s, sizeof(String));
 	gets_s(s, sizeof(String));*/
 	char a, b;
 	printf("input a:");
 	scanf_s("%c", &a, 2);
-=======
 	char a, b;
 	printf("input a:");
 	scanf_s("%c", &a, 2);
@@ -22,13 +20,9 @@ int main() {
 		printf("ÇëÖØÐÂÊäÈë£º");
 		scanf_s("%c", &a, 2);
 	}
->>>>>>> 90471926657ce6bfa739bf557e9cb626d79ce394
 	printf("%c\n", a);
 	printf("input b:");
 	scanf_s("%c", &b, 2);
 	printf("%c\n", b);
-<<<<<<< HEAD
 	return 0;
-=======
->>>>>>> 90471926657ce6bfa739bf557e9cb626d79ce394
 }
